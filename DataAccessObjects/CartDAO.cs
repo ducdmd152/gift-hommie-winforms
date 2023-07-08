62baac09c5692a37637fb9c56e6ccea254fb4cc0
thien123dh <134196273+thien123dh@users.noreply.github.com>
2023-07-08 22:42:04 +0700

    DONE DESIGN INTERFACE CART

---
+++ DataAccessObjects/CartDAO.cs
@@ -0,0 +1,51 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects
+{
+    public class CartDAO
+    {
+
+
+        // Singleton Pattern
+        private CartDAO() { }
+        private static CartDAO instance = null;
+        private static readonly object instanceLock = new object();
+        public static CartDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CartDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public List<Cart> GetAllCartItemsByUsername(string username)
+        {
+            try
+            {
+
+                using(var context = new HommieStoreContext())
+                {
+                    return context.Carts.Where(c => c.Username == username).ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
