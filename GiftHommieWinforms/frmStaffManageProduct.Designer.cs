4c17a638ec59e9a4fda4f96280e515af82b9018c
nguyentan <van59116@gmail.com>
2023-07-09 19:46:54 +0700

    design Product detail frm

--- GiftHommieWinforms/frmStaffManageProduct.Designer.cs
+++ GiftHommieWinforms/frmStaffManageProduct.Designer.cs
@@ -28,12 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmStaffManageProduct";
+            lbProductName = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            txtName = new System.Windows.Forms.TextBox();
+            txtPrice = new System.Windows.Forms.TextBox();
+            txtAvailable = new System.Windows.Forms.TextBox();
+            txtQuantity = new System.Windows.Forms.TextBox();
+            txtDesciption = new System.Windows.Forms.TextBox();
+            pbProductAvatar = new System.Windows.Forms.PictureBox();
+            btnSave = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            label6 = new System.Windows.Forms.Label();
+            txtImgUrl = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)pbProductAvatar).BeginInit();
+            SuspendLayout();
+            // 
+            // lbProductName
+            // 
+            lbProductName.AutoSize = true;
+            lbProductName.Font = new System.Drawing.Font("Segoe UI", 19.8000011F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lbProductName.Location = new System.Drawing.Point(308, 34);
+            lbProductName.Name = "lbProductName";
+            lbProductName.Size = new System.Drawing.Size(250, 46);
+            lbProductName.TabIndex = 0;
+            lbProductName.Text = "Product Name";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(38, 85);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(64, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(38, 157);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(54, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Price";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(38, 241);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(92, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Available";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(38, 318);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(88, 28);
+            label4.TabIndex = 4;
+            label4.Text = "Quantity";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label5.Location = new System.Drawing.Point(38, 394);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(112, 28);
+            label5.TabIndex = 5;
+            label5.Text = "Description";
+            // 
+            // txtName
+            // 
+            txtName.Location = new System.Drawing.Point(175, 89);
+            txtName.Name = "txtName";
+            txtName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            txtName.Size = new System.Drawing.Size(195, 27);
+            txtName.TabIndex = 6;
+            txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPrice
+            // 
+            txtPrice.Location = new System.Drawing.Point(175, 161);
+            txtPrice.Name = "txtPrice";
+            txtPrice.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            txtPrice.Size = new System.Drawing.Size(195, 27);
+            txtPrice.TabIndex = 7;
+            txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAvailable
+            // 
+            txtAvailable.Location = new System.Drawing.Point(175, 241);
+            txtAvailable.Name = "txtAvailable";
+            txtAvailable.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            txtAvailable.Size = new System.Drawing.Size(195, 27);
+            txtAvailable.TabIndex = 8;
+            txtAvailable.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.Location = new System.Drawing.Point(175, 318);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            txtQuantity.Size = new System.Drawing.Size(195, 27);
+            txtQuantity.TabIndex = 9;
+            txtQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDesciption
+            // 
+            txtDesciption.Location = new System.Drawing.Point(175, 398);
+            txtDesciption.Multiline = true;
+            txtDesciption.Name = "txtDesciption";
+            txtDesciption.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            txtDesciption.Size = new System.Drawing.Size(622, 100);
+            txtDesciption.TabIndex = 0;
+            txtDesciption.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pbProductAvatar
+            // 
+            pbProductAvatar.Location = new System.Drawing.Point(557, 89);
+            pbProductAvatar.Name = "pbProductAvatar";
+            pbProductAvatar.Size = new System.Drawing.Size(240, 271);
+            pbProductAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pbProductAvatar.TabIndex = 86;
+            pbProductAvatar.TabStop = false;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnSave.Location = new System.Drawing.Point(281, 590);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(100, 44);
+            btnSave.TabIndex = 87;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(491, 590);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(100, 44);
+            button1.TabIndex = 88;
+            button1.Text = "Close";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label6.Location = new System.Drawing.Point(42, 529);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(97, 28);
+            label6.TabIndex = 89;
+            label6.Text = "Image Url";
+            // 
+            // txtImgUrl
+            // 
+            txtImgUrl.Location = new System.Drawing.Point(175, 533);
+            txtImgUrl.Name = "txtImgUrl";
+            txtImgUrl.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            txtImgUrl.Size = new System.Drawing.Size(343, 27);
+            txtImgUrl.TabIndex = 90;
+            txtImgUrl.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmStaffManageProduct
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(957, 683);
+            Controls.Add(txtImgUrl);
+            Controls.Add(label6);
+            Controls.Add(button1);
+            Controls.Add(btnSave);
+            Controls.Add(pbProductAvatar);
+            Controls.Add(txtDesciption);
+            Controls.Add(txtQuantity);
+            Controls.Add(txtAvailable);
+            Controls.Add(txtPrice);
+            Controls.Add(txtName);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lbProductName);
+            Name = "frmStaffManageProduct";
+            Text = "frmStaffManageProduct";
+            ((System.ComponentModel.ISupportInitialize)pbProductAvatar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbProductName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtAvailable;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtDesciption;
+        private System.Windows.Forms.PictureBox pbProductAvatar;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtImgUrl;
     }
 }
